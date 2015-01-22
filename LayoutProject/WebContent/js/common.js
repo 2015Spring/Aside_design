09bb7633eb5fbcc987b84ab0e7224a1925a43383
eunhee <behi96@gmail.com>
2015-01-22 15:03:30 +0900

    login.html common.css
    글자 적용시키는거 하고있었음!

--- LayoutProject/WebContent/js/common.js
+++ LayoutProject/WebContent/js/common.js
@@ -20,7 +20,7 @@ function adjustStyle(width, height) {
     	$('#logo_area').css('margin', '0 auto').css('width', '154px').css('max-width', '154px');
     	$('#logo').css('width', '154px');
     	$('#account').css('margin', '0 auto').css('width', '350px').css('max-width', '350px');
-    	$('#welcome').css('margin', '0 auto').css('width', '385px').css('max-width', '385px');
+    	$('#welcome').css('margin', '0 auto').css('width', '375px').css('max-width', '375px');
     }
     
     if(height < 420) {
